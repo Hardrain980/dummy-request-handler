f6c48a9bfbce3b09b2b72718b440afe05ce07e6f
Leo <i@setuid0.dev>
2020-12-31 14:00:49 +0800

    Implement Leo\Fixtures\DummyRequestHandler

---
+++ src/DummyRequestHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Leo\Fixtures;
+
+use \Psr\Http\Server\RequestHandlerInterface;
+use \Psr\Http\Message\ServerRequestInterface;
+use \Psr\Http\Message\ResponseInterface;
+use \GuzzleHttp\Psr7;
+
+class DummyRequestHandler implements RequestHandlerInterface
+{
+	/**
+	 * Request passed in
+	 * @var \Psr\Http\Message\ServerRequestInterface
+	 */
+	private $request;
+
+	/**
+	 * Response to be returned
+	 * @var \Psr\Http\Message\ResponseInterface
+	 */
+	private $response;
+
+	public function __construct(
+		?ResponseInterface $response = null
+	)
+	{
+		$this->response = $response ?? new Psr7\Response();
+	}
+
+	public function handle(ServerRequestInterface $request):ResponseInterface
+	{
+		$this->request = $request;
+
+		return $this->response;
+	}
+
+	public function getRequest():ServerRequestInterface
+	{
+		return $this->request;
+	}
+}
+
+?>
